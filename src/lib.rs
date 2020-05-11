00a989130cb06ee5256b919a499b2a18ceb7acb4
rkr35 <rkr35@drexel.edu>
2020-05-11 10:53:31 -0400

    Add initial code w/ std enabled

---
+++ src/lib.rs
@@ -0,0 +1,33 @@
+use core::ptr;
+
+use winapi::{
+    shared::minwindef::{BOOL, DWORD, HINSTANCE, LPVOID, TRUE},
+    um::{
+        libloaderapi::{DisableThreadLibraryCalls, FreeLibraryAndExitThread},
+        processthreadsapi::CreateThread,
+        synchapi::Sleep,
+        winnt::DLL_PROCESS_ATTACH,
+    },
+};
+
+extern "system" fn on_attach(dll: LPVOID) -> DWORD {
+    unsafe {
+        Sleep(1000);
+        FreeLibraryAndExitThread(dll.cast(), 0);
+    }
+
+    0
+}
+
+#[no_mangle]
+#[allow(non_snake_case)]
+extern "system" fn DllMain(dll: HINSTANCE, reason: DWORD, _: LPVOID) -> BOOL {
+    if reason == DLL_PROCESS_ATTACH {
+        unsafe {
+            DisableThreadLibraryCalls(dll);
+            CreateThread(ptr::null_mut(), 0, Some(on_attach), dll.cast(), 0, ptr::null_mut());
+        }
+    }
+
+    TRUE
+}
